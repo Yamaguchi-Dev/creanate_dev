4a0ea662d1c726d74111fd98db34513cef550d82
Yamaguchi-Dev <kei.yamaguchi.0104@gmail.com>
2022-02-21 14:59:31 +0900

    テスト用メールアドレス変更

--- mail_admin.php
+++ mail_admin.php
@@ -2,7 +2,8 @@
 function get_admin_mail_data($input)
 {
 $mail_data_arr = array();
-$mail_data_arr["to"] = "kei.yamaguchi.0104@gmail.com";
+$mail_data_arr["to"] = "horibe@favor.co.jp";
+//$mail_data_arr["to"] = "kei.yamaguchi.0104@gmail.com";
 //$mail_data_arr["to"] = "inquiry@creanate.co.jp";
 $mail_data_arr["subject"] = "クレアネイト株式会社お問い合わせフォームより";
 $headers = "From: クレアネイト株式会社 お問い合わせフォーム<inquiry@creanate.co.jp>";
